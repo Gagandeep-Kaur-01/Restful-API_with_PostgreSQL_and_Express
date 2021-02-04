85d7e9af621ace84b0d28cb3c177013549b87e48
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 10:59:15 +0530

    to get access for json data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const pool = require("./db");
 
+app.use(express.json()) // => req.body : to get access on body of json data
+
 app.listen(5000, () => {
     console.log("server is listening on port: 5000");
 });
