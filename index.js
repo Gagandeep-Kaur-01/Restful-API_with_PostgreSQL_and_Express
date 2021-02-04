e026ac947b25cf4bcf93bcbef10d29eac10afaba
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 11:01:40 +0530

    defined the routes

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const pool = require("./db");
 
 app.use(express.json()) // => req.body : to get access on body of json data
 
+//-------------- Routes -----------------------//
+
+// get all
+
+// get a single
+
+// create
+
+// update
+
+// delete 
+
 app.listen(5000, () => {
     console.log("server is listening on port: 5000");
 });
