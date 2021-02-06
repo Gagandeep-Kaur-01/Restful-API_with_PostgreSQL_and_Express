1ac74dbebc2c426bc8773d5c882c8d8c11c5a5c6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-06 09:46:04 +0530

    created delete request, deleting data of specific id

--- index.js
+++ index.js
@@ -72,6 +72,18 @@ app.put("/update/:id", async(req, res) => {
 })
 
 // delete 
+app.delete("/delete/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query(
+            "DELETE FROM practice WHERE prac_id = $1", 
+            [id]
+        );
+        res.json("Data was deleted!");
+    } catch (err) {
+        console.log(err.message)
+    }
+})
 
 app.listen(5000, () => {
     console.log("server is listening on port: 5000");
